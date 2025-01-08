3452d47eea24b911c86d34feb2a5a0575e6f6abb
nicolassutter <contact.sutter.nicolas@gmail.com>
2025-01-08 14:20:57 +0100

    feat: also fetch japanese games for a complete library

--- src/lib/lib.ts
+++ src/lib/lib.ts
@@ -2,16 +2,28 @@ import { gamesTable } from "../db/schema";
 import { RawGame } from "../../types";
 import { db } from "../db/db";
 
-const GAMES_DATABASE_URL =
-  "https://raw.githubusercontent.com/blawar/titledb/master/US.en.json";
+const GAMES_DATABASE_URLS = [
+  "https://raw.githubusercontent.com/blawar/titledb/master/JP.ja.json",
+  "https://raw.githubusercontent.com/blawar/titledb/master/GB.en.json",
+];
 
 export const fetchGames = async () => {
   console.log("Querying games");
   console.time("fetchGames");
 
   try {
-    const data: Record<string, RawGame> = await fetch(GAMES_DATABASE_URL).then(
-      (response) => response.json()
+    const data = await Promise.all(
+      GAMES_DATABASE_URLS.map(async (url) => {
+        return fetch(url)
+          .catch((error) => {
+            console.warn("Error fetching games", url, error);
+            return null;
+          })
+          .then((response) => response?.json()) as Promise<Record<
+          string,
+          RawGame
+        > | null>;
+      })
     );
 
     console.log("Games fetched");
@@ -32,33 +44,35 @@ export async function updateDatabase() {
     console.time("updateDatabase");
 
     await db.transaction(async (tx) => {
-      for (const rawGame of Object.values(games)) {
-        if (!rawGame.id) continue;
+      for (const subGames of games) {
+        for (const rawGame of Object.values(subGames ?? {})) {
+          if (!rawGame.id) continue;
 
-        let year, month, day;
+          let year, month, day;
 
-        if (rawGame.releaseDate) {
-          const date = `${rawGame.releaseDate}`;
-          year = Number(date.slice(0, 4));
-          month = Number(date.slice(4, 6));
-          day = Number(date.slice(6, 8));
+          if (rawGame.releaseDate) {
+            const date = `${rawGame.releaseDate}`;
+            year = Number(date.slice(0, 4));
+            month = Number(date.slice(4, 6));
+            day = Number(date.slice(6, 8));
+          }
+
+          const game: typeof gamesTable.$inferInsert = {
+            bannerUrl: rawGame.bannerUrl,
+            category: rawGame.category,
+            name: rawGame.name,
+            releaseDate:
+              year && month && day
+                ? new Date(year, month, day).toISOString()
+                : null,
+            titleId: rawGame.id,
+          };
+
+          await tx.insert(gamesTable).values(game).onConflictDoUpdate({
+            target: gamesTable.titleId,
+            set: game,
+          });
         }
-
-        const game: typeof gamesTable.$inferInsert = {
-          bannerUrl: rawGame.bannerUrl,
-          category: rawGame.category,
-          name: rawGame.name,
-          releaseDate:
-            year && month && day
-              ? new Date(year, month, day).toISOString()
-              : null,
-          titleId: rawGame.id,
-        };
-
-        await tx.insert(gamesTable).values(game).onConflictDoUpdate({
-          target: gamesTable.titleId,
-          set: game,
-        });
       }
     });
 
