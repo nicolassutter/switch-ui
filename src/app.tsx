b287717f4bdacf2f14841ec9290e9459f449e44b
nicolassutter <contact.sutter.nicolas@gmail.com>
2025-01-08 11:15:33 +0100

    feat: add error boundary to app

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,7 @@ import "virtual:uno.css";
 import "./app.css";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
-import { Suspense } from "solid-js";
+import { ErrorBoundary, Suspense } from "solid-js";
 import { ColorModeProvider, ColorModeScript } from "@kobalte/core";
 import { MetaProvider, Title } from "@solidjs/meta";
 
@@ -16,7 +16,13 @@ export default function App() {
             <Title>Switch UI</Title>
             <Suspense>
               <ColorModeScript />
-              <ColorModeProvider>{props.children}</ColorModeProvider>
+              <ColorModeProvider>
+                <ErrorBoundary
+                  fallback={(err) => <div>Error: {err.message}</div>}
+                >
+                  {props.children}
+                </ErrorBoundary>
+              </ColorModeProvider>
             </Suspense>
           </MetaProvider>
         </>
